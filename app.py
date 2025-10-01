d3cf1b048d796f5bc33545bf5d27dbf1d81136fd
omarzeidan89 <omar.zedan56@gmail.com>
2025-10-02 01:58:35 +0300

    Update app.py

--- app.py
+++ app.py
@@ -502,4 +502,5 @@ def health():
 # ------------ Run ------------
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=5000)  # debug=False تلقائيًا
+
